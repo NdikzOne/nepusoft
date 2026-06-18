e3cf214f1b23e5488377c349eb5c492ba49afb11
Nd <baksocloud@gmail.com>
2026-06-18 07:53:11 +0700

    Update google.js

--- api/v1/auth/google.js
+++ api/v1/auth/google.js
@@ -1,16 +1,22 @@
 import { OAuth2Client } from 'google-auth-library';
 
 export default async function handler(req, res) {
-  const oauth2Client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:5173'}/api/v1/auth/callback`
-  );
+const redirectUri = "${process.env.APP_URL}/api/v1/auth/callback";
 
-  const authorizeUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
-  });
+const oauth2Client = new OAuth2Client(
+process.env.GOOGLE_CLIENT_ID,
+process.env.GOOGLE_CLIENT_SECRET,
+redirectUri
+);
 
-  res.redirect(authorizeUrl);
+const authorizeUrl = oauth2Client.generateAuthUrl({
+access_type: 'offline',
+prompt: 'consent',
+scope: [
+'https://www.googleapis.com/auth/userinfo.profile',
+'https://www.googleapis.com/auth/userinfo.email'
+]
+});
+
+res.redirect(authorizeUrl);
 }
